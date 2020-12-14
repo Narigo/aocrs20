b29de7c31bb99a8b5d156fd7fddc28eeb7864e8a
Joern Bernhardt <joern.bernhardt@compose.us>
2020-12-15 00:05:02 +0100

    Add second star for day 05
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/day_05/mod.rs
+++ src/day_05/mod.rs
@@ -8,30 +8,38 @@ fn get_seat(input: &str) -> Seat {
     let mut row_min: u32 = 0;
     let mut row_max: u32 = 127;
     for c in input.get(0..7).unwrap().chars() {
-        println!("row_min {}, row_max {}, c {}", row_min, row_max, c);
         match c {
             'F' => row_max = row_min + ((row_max - row_min) / 2),
             'B' => row_min = row_min + ((row_max - row_min + 1) / 2),
             _ => panic!("Needs to get 7 F or B chars first!"),
         }
     }
-    println!("row_min {}, row_max {}", row_min, row_max);
     let mut col_min: u32 = 0;
     let mut col_max: u32 = 7;
     for c in input.get(7..10).unwrap().chars() {
-        println!("col_min {}, col_max {}, c {}", col_min, col_max, c);
         match c {
             'L' => col_max = col_min + ((col_max - col_min) / 2),
             'R' => col_min = col_min + ((col_max - col_min + 1) / 2),
             _ => panic!("Needs to get 3 R or L chars in the end!"),
         }
     }
-    println!("col_min {}, col_max {}", col_min, col_max);
     Seat {
         row: row_min,
         column: col_min,
         id: row_min * 8 + col_min,
     }
+}
+
+fn free_seat(mut seat_ids: Vec<u32>) -> u32 {
+    seat_ids.sort();
+    let mut a = seat_ids.get(0).unwrap();
+    for n in seat_ids.iter() {
+        if a < &(n - 1) {
+            return n - 1;
+        }
+        a = n;
+    }
+    panic!("Could not find an answer");
 }
 
 #[cfg(test)]
@@ -81,5 +89,13 @@ mod test {
         let lines = file.lines();
         let result = lines.map(get_seat).max_by_key(|seat| seat.id).unwrap();
         assert_eq!(991, result.id, "highest seat id should be 991");
+    }
+
+    #[test]
+    fn check_free_seat() {
+        let file = read_file("./src/day_05/input.txt");
+        let lines = file.lines();
+        let seat_ids = lines.map(get_seat).map(|s| s.id).collect::<Vec<u32>>();
+        assert_eq!(534, free_seat(seat_ids));
     }
 }
