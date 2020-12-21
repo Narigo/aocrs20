5bf3a2e3b06d5d14f1c8a44b9d35c6a75ad88d84
Joern Bernhardt <joern.bernhardt@compose.us>
2020-12-21 11:08:17 +0100

    Add implementation for second star example
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/day_06/mod.rs
+++ src/day_06/mod.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::collections::HashSet;
 
 fn amount_of_questions_answered(input: Vec<String>) -> u32 {
@@ -9,6 +10,23 @@ fn amount_of_questions_answered(input: Vec<String>) -> u32 {
             unique_chars.insert(c);
         }
         result = result + unique_chars.len();
+    }
+    result as u32
+}
+
+fn amount_of_questions_answered_by_all(input: Vec<String>) -> u32 {
+    let mut result = 0;
+    for part in input {
+        let mut answers = HashMap::<char, usize>::new();
+        let mut amount_of_people = 0;
+        for answer in part.lines() {
+            amount_of_people += 1;
+            for a in answer.chars() {
+                *answers.entry(a).or_insert(0) += 1;
+            }
+        }
+        answers.retain(|_k, v| *v == amount_of_people);
+        result += answers.len();
     }
     result as u32
 }
@@ -35,5 +53,13 @@ mod test {
         let splitted = split_into_parts(&file);
         let result = amount_of_questions_answered(splitted);
         assert_eq!(6551, result);
+    }
+
+    #[test]
+    fn check_example_for_all() {
+        let file = read_file("./src/day_06/example.txt");
+        let splitted = split_into_parts(&file);
+        let result = amount_of_questions_answered_by_all(splitted);
+        assert_eq!(6, result);
     }
 }
