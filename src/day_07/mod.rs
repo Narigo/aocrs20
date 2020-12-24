87861ecd5d008640b2299702413161458777c556
Joern Bernhardt <joern.bernhardt@compose.us>
2020-12-24 14:33:16 +0100

    Add split for bags
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/day_07/mod.rs
+++ src/day_07/mod.rs
@@ -1,15 +1,34 @@
+use regex::Regex;
 use std::collections::HashMap;
 
 type ColoredBag = String;
 type RuleSet = HashMap<ColoredBag, Vec<(u32, ColoredBag)>>;
 
-fn file_to_rule_set(input: &String) -> RuleSet {
+fn file_to_rule_set(input: &String) -> Result<RuleSet, String> {
     let mut rules = RuleSet::new();
+    let start_re = Regex::new(r"^(.*?) bags contain (.*)\.$").unwrap();
+    let bag_re = Regex::new(r"^(\d+) (.*)\s*bags?$").unwrap();
     for line in input.lines() {
-        let re = "^(.*?) bags contain (.*).$";
-        rules.insert(format!("key-{}", line), vec![(1, "???".to_owned())]);
+        match start_re.captures(line) {
+            Some(groups) => {
+                let key = groups
+                    .get(1)
+                    .expect("Should have a group 1")
+                    .as_str()
+                    .to_owned();
+                let values = groups
+                    .get(2)
+                    .expect("Should have a group 2")
+                    .as_str()
+                    .split(",")
+                    .map(|s| s.trim())
+                    .map(|s| (1, s.to_owned()));
+                rules.insert(key, values.collect());
+            }
+            None => {}
+        }
     }
-    rules
+    Ok(rules)
 }
 
 #[cfg(test)]
@@ -20,8 +39,8 @@ mod test {
     #[test]
     fn check_example() {
         let file = read_file("./src/day_07/example.txt");
-        let rules = file_to_rule_set(&file);
-        println!("{}", file);
+        let rules = file_to_rule_set(&file).expect("Should be possible to read as rules");
+        println!("{:?}", rules);
         assert_eq!(9, rules.len());
     }
 }
