44a6df1cb1cf9f5dba46d6ee9f251bd8b06c16e3
Joern Bernhardt <joern.bernhardt@compose.us>
2020-12-25 15:19:04 +0100

    Split amount and colors
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/day_07/mod.rs
+++ src/day_07/mod.rs
@@ -4,28 +4,49 @@ use std::collections::HashMap;
 type ColoredBag = String;
 type RuleSet = HashMap<ColoredBag, Vec<(u32, ColoredBag)>>;
 
+fn split_amount_and_color(input: &str) -> Result<(u32, String), String> {
+    let bag_re = Regex::new(r"^(\d+) (.*)\s*bags?$").unwrap();
+    bag_re
+        .captures(input)
+        .map(|groups| {
+            (
+                groups
+                    .get(1)
+                    .expect("Should have an integer amount of bags")
+                    .as_str()
+                    .parse::<u32>()
+                    .expect("Should be able to parse an unsigned integer"),
+                groups
+                    .get(2)
+                    .expect("Should have a bag color")
+                    .as_str()
+                    .to_owned(),
+            )
+        })
+        .ok_or(format!("Could not capture a group on '{}'", input))
+}
+
 fn file_to_rule_set(input: &String) -> Result<RuleSet, String> {
     let mut rules = RuleSet::new();
     let start_re = Regex::new(r"^(.*?) bags contain (.*)\.$").unwrap();
-    let bag_re = Regex::new(r"^(\d+) (.*)\s*bags?$").unwrap();
     for line in input.lines() {
-        match start_re.captures(line) {
-            Some(groups) => {
-                let key = groups
-                    .get(1)
-                    .expect("Should have a group 1")
-                    .as_str()
-                    .to_owned();
-                let values = groups
-                    .get(2)
-                    .expect("Should have a group 2")
-                    .as_str()
-                    .split(",")
-                    .map(|s| s.trim())
-                    .map(|s| (1, s.to_owned()));
-                rules.insert(key, values.collect());
+        for groups in start_re.captures(line) {
+            let key = groups
+                .get(1)
+                .expect("Should have a group 1")
+                .as_str()
+                .to_owned();
+            let second_part = groups.get(2).expect("Should have a group 2").as_str();
+            if second_part == "no other bags" {
+                rules.insert(key, vec![]);
+                continue;
             }
-            None => {}
+            let values = second_part
+                .split(",")
+                .map(|s| s.trim())
+                .map(|s| split_amount_and_color(s).expect("Could not split into amount and color"))
+                .collect();
+            rules.insert(key, values);
         }
     }
     Ok(rules)
