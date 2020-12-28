b6b85757d8d54ca351b79e770c6b644295061d0b
Joern Bernhardt <joern.bernhardt@compose.us>
2020-12-28 11:23:03 +0100

    Add example 2
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/day_07/mod.rs
+++ src/day_07/mod.rs
@@ -58,9 +58,7 @@ fn contained_by_from_rules(rules: &RuleSet, color: &ColoredBag) -> ContainedBySe
     let mut values: ContainedBySet = HashSet::new();
     for entry in rules.iter() {
         let entry_values = entry.1;
-        println!("entry_values: {:?}", entry_values);
         for colored_bag in entry_values {
-            println!("colored_bag: {:?}", colored_bag);
             if colored_bag.1.eq(color) {
                 values.insert(entry.0.clone());
                 let contained_by = contained_by_from_rules(rules, entry.0);
@@ -69,6 +67,15 @@ fn contained_by_from_rules(rules: &RuleSet, color: &ColoredBag) -> ContainedBySe
         }
     }
     values
+}
+
+fn amount_of_bags_necessary(rules: &RuleSet, color: &ColoredBag) -> u32 {
+    let contained_bags = rules.get(color).unwrap();
+    let mut result = 1;
+    for amount_and_bag in contained_bags.iter() {
+        result = result + amount_and_bag.0 * amount_of_bags_necessary(rules, &amount_and_bag.1);
+    }
+    result
 }
 
 #[cfg(test)]
@@ -80,7 +87,6 @@ mod test {
     fn check_rules_set() {
         let file = read_file("./src/day_07/example.txt");
         let rules = file_to_rule_set(&file).expect("Should be possible to read as rules");
-        println!("{:?}", rules);
         assert_eq!(9, rules.len());
     }
 
@@ -89,7 +95,30 @@ mod test {
         let file = read_file("./src/day_07/example.txt");
         let rules = file_to_rule_set(&file).expect("Should be possible to read as rules");
         let contained_by = contained_by_from_rules(&rules, &"shiny gold".to_owned());
-        println!("{:?}", contained_by);
         assert_eq!(4, contained_by.len());
+    }
+
+    #[test]
+    fn check_input() {
+        let file = read_file("./src/day_07/input.txt");
+        let rules = file_to_rule_set(&file).expect("Should be possible to read as rules");
+        let contained_by = contained_by_from_rules(&rules, &"shiny gold".to_owned());
+        assert_eq!(151, contained_by.len());
+    }
+
+    #[test]
+    fn check_example_1_for_star_2() {
+        let file = read_file("./src/day_07/example.txt");
+        let rules = file_to_rule_set(&file).expect("Should be possible to read as rules");
+        let result = amount_of_bags_necessary(&rules, &"shiny gold".to_owned()) - 1;
+        assert_eq!(32, result);
+    }
+
+    #[test]
+    fn check_example_2_for_star_2() {
+        let file = read_file("./src/day_07/example2.txt");
+        let rules = file_to_rule_set(&file).expect("Should be possible to read as rules");
+        let result = amount_of_bags_necessary(&rules, &"shiny gold".to_owned()) - 1;
+        assert_eq!(126, result);
     }
 }
