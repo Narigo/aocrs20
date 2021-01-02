3c7f6f635bff446ba1a7c8cd497dc07a64e0de20
Joern Bernhardt <joern.bernhardt@compose.us>
2021-01-03 00:35:01 +0100

    Add example star 2 and find_corrupt_instruction
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/day_08/mod.rs
+++ src/day_08/mod.rs
@@ -1,10 +1,10 @@
 use std::collections::HashSet;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Instruction {
     Acc(i32),
     Jmp(i32),
-    Nop,
+    Nop(i32),
 }
 
 fn line_to_instruction(input: &str) -> Instruction {
@@ -13,7 +13,7 @@ fn line_to_instruction(input: &str) -> Instruction {
     match command {
         "acc" => Instruction::Acc(arg.parse::<i32>().expect("Should have an integer")),
         "jmp" => Instruction::Jmp(arg.parse::<i32>().expect("Should have an integer")),
-        "nop" => Instruction::Nop,
+        "nop" => Instruction::Nop(arg.parse::<i32>().expect("Should have an integer")),
         _ => panic!("Got a non acc, jmp or nop command!"),
     }
 }
@@ -27,8 +27,30 @@ fn file_to_instructions(input: &str) -> Vec<Instruction> {
     instructions
 }
 
+fn replace_instruction(old_instruction: Instruction) -> Instruction {
+    match old_instruction {
+        Instruction::Jmp(count) => Instruction::Nop(count),
+        Instruction::Nop(count) => Instruction::Jmp(count),
+        other => other,
+    }
+}
+
 fn find_corrupt_instruction(instructions: Vec<Instruction>) -> Result<i32, i32> {
-    Ok(0)
+    let mut current_mutated_line: usize = 0;
+    let max_line = instructions.len();
+    loop {
+        let mut copied = instructions.clone();
+        let old_instruction = copied[current_mutated_line].clone();
+        std::mem::replace(
+            &mut copied[current_mutated_line],
+            replace_instruction(old_instruction),
+        );
+        let result = run_instructions_until_loop(copied);
+        if result.is_ok() || max_line <= current_mutated_line {
+            return result;
+        }
+        current_mutated_line += 1;
+    }
 }
 
 fn run_instructions_until_loop(instructions: Vec<Instruction>) -> Result<i32, i32> {
@@ -48,7 +70,7 @@ fn run_instructions_until_loop(instructions: Vec<Instruction>) -> Result<i32, i3
             Instruction::Jmp(count) => {
                 current_line = (count + (current_line as i32)) as usize;
             }
-            Instruction::Nop => {
+            Instruction::Nop(_) => {
                 current_line = current_line + 1;
             }
         }
@@ -85,6 +107,6 @@ mod test {
         let file = read_file("./src/day_08/example.txt");
         let instructions = file_to_instructions(&file);
         let last_state = find_corrupt_instruction(instructions);
-        assert_eq!(Ok(6), last_state);
+        assert_eq!(Ok(8), last_state);
     }
 }
