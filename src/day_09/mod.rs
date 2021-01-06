8e205267c2fc23f69033c60d1990bc6ae6256836
Joern Bernhardt <joern.bernhardt@compose.us>
2021-01-06 13:59:53 +0100

    Add solution for first example
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/day_09/mod.rs
+++ src/day_09/mod.rs
@@ -1,9 +1,38 @@
-fn check_valid_number(preamble: &Vec<u32>, next_number: u32) -> bool {
+fn check_valid_number(preamble: &Vec<u32>, number: &u32) -> bool {
+    for a in preamble.iter() {
+        for b in preamble.iter() {
+            if a == b {
+                continue;
+            }
+            if a + b == *number {
+                return true;
+            }
+        }
+    }
     false
 }
 
 fn find_first_invalid(input: &str, preamble_length: usize) -> u32 {
-    127
+    let numbers: Vec<u32> = input
+        .lines()
+        .map(|s| s.parse::<u32>().expect("should be numbers"))
+        .collect();
+    for i in preamble_length..numbers.len() {
+        let number = numbers.get(i).unwrap();
+        let to_check: Vec<u32> = numbers
+            .clone()
+            .into_iter()
+            .skip(i - preamble_length)
+            .take(preamble_length)
+            .collect();
+        if check_valid_number(&to_check, number) {
+            println!("{} is okay", number);
+        } else {
+            println!("{} is NOT okay", number);
+            return *number;
+        }
+    }
+    0
 }
 
 #[cfg(test)]
@@ -12,8 +41,8 @@ mod test {
     use crate::util::*;
 
     #[test]
-    fn check_example() {
-        let file = read_file("./src/day_08/example.txt");
+    fn check_example_day_09() {
+        let file = read_file("./src/day_09/example.txt");
         let result = find_first_invalid(&file, 5);
         assert_eq!(127, result);
     }
