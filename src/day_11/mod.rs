06cd445d16e96196cf04eb535ab80a9cd67b381c
Joern Bernhardt <joern.bernhardt@compose.us>
2021-01-21 11:28:15 +0100

    Add function to get amount of occupied adjacent cells
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/day_11/mod.rs
+++ src/day_11/mod.rs
@@ -34,6 +34,41 @@ impl fmt::Display for Grid {
     }
 }
 
+trait GridRules {
+    fn get_occupied_adjacent_cells(&self, x: usize, y: usize) -> usize;
+}
+
+impl GridRules for Grid {
+    fn get_occupied_adjacent_cells(&self, x: usize, y: usize) -> usize {
+        let possible_cells = vec![
+            y.checked_sub(1)
+                .and_then(|y| self.cells.get(y))
+                .and_then(|row| x.checked_sub(1).and_then(|x| row.get(x))),
+            y.checked_sub(1)
+                .and_then(|y| self.cells.get(y))
+                .and_then(|row| row.get(x)),
+            y.checked_sub(1)
+                .and_then(|y| self.cells.get(y))
+                .and_then(|row| row.get(x + 1)),
+            self.cells
+                .get(y)
+                .and_then(|row| x.checked_sub(1).and_then(|x| row.get(x))),
+            self.cells.get(y).and_then(|row| row.get(x + 1)),
+            self.cells
+                .get(y + 1)
+                .and_then(|row| x.checked_sub(1).and_then(|x| row.get(x))),
+            self.cells.get(y + 1).and_then(|row| row.get(x)),
+            self.cells.get(y + 1).and_then(|row| row.get(x + 1)),
+        ];
+        possible_cells.iter().fold(0, |acc, c| {
+            acc + match c {
+                Some(Cell::Occupied) => 1,
+                _ => 0,
+            }
+        })
+    }
+}
+
 fn input_to_grid(input: &str) -> Grid {
     let lines = input.lines().collect::<Vec<&str>>();
     let height = lines.len();
@@ -71,6 +106,16 @@ mod test {
         println!("{}", grid);
         assert_eq!(10, grid.height);
         assert_eq!(10, grid.width);
+    }
+
+    #[test]
+    fn check_adjacent_cells() {
+        let file = read_file("./src/day_11/adjacent_cells.txt");
+        let grid = input_to_grid(&file);
+        assert_eq!(3, grid.get_occupied_adjacent_cells(1, 1));
+        assert_eq!(2, grid.get_occupied_adjacent_cells(0, 0));
+        assert_eq!(4, grid.get_occupied_adjacent_cells(1, 0));
+        assert_eq!(2, grid.get_occupied_adjacent_cells(3, 2));
     }
 
     #[test]
