2457c61d204bc7764baa514c32d6d7c15cf8f241
Joern Bernhardt <joern.bernhardt@compose.us>
2021-02-10 21:53:08 +0100

    Add day 13 stub
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

---
+++ src/day_13/mod.rs
@@ -0,0 +1,15 @@
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::util::*;
+
+    #[test]
+    fn check_day_13() {}
+
+    #[test]
+    fn check_day_13_star1() {}
+
+    #[test]
+    fn check_day_13_star2() {}
+}
