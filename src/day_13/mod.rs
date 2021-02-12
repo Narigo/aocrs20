b909dd719ca2fadd7a2d076d5d2e15fde9a1f9f7
Joern Bernhardt <joern.bernhardt@compose.us>
2021-02-12 12:12:36 +0100

    Add from_input and try out for how to calculate it
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/day_13/mod.rs
+++ src/day_13/mod.rs
@@ -1,13 +1,32 @@
 type Bus = usize;
 type ArrivalTime = usize;
 
-fn from_input(input: &str) -> (ArrivalTime, Vec<Bus>) {
-    let lines = input.lines();
-    (lines.get(0).unwrap().parse::<usize>(), lines.get(1).unwrap().split(",").filter(|c| c != 'x').parse::<usize>())
+fn from_input(input: &String) -> (ArrivalTime, Vec<Bus>) {
+    let mut lines = input.lines();
+    (
+        lines.next().unwrap().parse::<usize>().unwrap(),
+        lines
+            .next()
+            .unwrap()
+            .split(",")
+            .filter(|c| *c != "x")
+            .map(|c| c.parse::<usize>().unwrap())
+            .collect(),
+    )
 }
 
 fn get_earliest_bus(arrival_time: ArrivalTime, busses: Vec<Bus>) -> Bus {
-    59
+    let mut best_bus = busses[0];
+    for bus in busses[1..].iter() {
+        let time_before = best_bus * (arrival_time / best_bus + 1) % best_bus;
+        let time_this = bus * (arrival_time / bus) % bus;
+        println!("best_bus = {}, time = {}", best_bus, time_before);
+        println!("     bus = {}, time = {}", bus, time_this);
+        if time_this < time_before {
+            best_bus = *bus;
+        }
+    }
+    best_bus
 }
 
 #[cfg(test)]
@@ -25,15 +44,18 @@ mod test {
     fn check_day_13_star2() {}
 
     #[test]
-    fn check_day_13_star1_from_input() {
+    fn check_day_13_star1_example_input_values() {
         let file = read_file("./src/day_13/example.txt");
-        let (arrival_time, busses) = from_input(file);
-        assert_eq(939, arrival_time);
-        assert_eq(vec![7,13,59,31,19], busses);
+        let (arrival_time, busses) = from_input(&file);
+        assert_eq!(939, arrival_time);
+        assert_eq!(vec![7, 13, 59, 31, 19], busses);
     }
 
+    #[test]
     fn check_day_13_star1_example() {
         let file = read_file("./src/day_13/example.txt");
+        let (arrival_time, busses) = from_input(&file);
+        let earliest = get_earliest_bus(arrival_time, busses);
+        assert_eq!(59, earliest);
     }
-
 }
