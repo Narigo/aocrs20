0f2535876cc6855d88c89a79b05af5a5c62fabc2
Joern Bernhardt <joern.bernhardt@compose.us>
2021-02-15 00:22:32 +0100

    Use None to encode whether value was x
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/day_13/mod.rs
+++ src/day_13/mod.rs
@@ -2,7 +2,7 @@ type Bus = usize;
 type ArrivalTime = usize;
 type WaitTime = usize;
 
-fn from_input(input: &String) -> (ArrivalTime, Vec<Bus>) {
+fn from_input(input: &String) -> (ArrivalTime, Vec<Option<Bus>>) {
     let mut lines = input.lines();
     (
         lines.next().unwrap().parse::<usize>().unwrap(),
@@ -10,17 +10,20 @@ fn from_input(input: &String) -> (ArrivalTime, Vec<Bus>) {
             .next()
             .unwrap()
             .split(",")
-            .filter(|c| *c != "x")
-            .map(|c| c.parse::<usize>().unwrap())
+            .map(|c| c.parse::<usize>().ok())
             .collect(),
     )
 }
 
-fn get_earliest_bus(arrival_time: ArrivalTime, busses: Vec<Bus>) -> (Bus, WaitTime) {
-    let mut best_bus = busses[0];
+fn get_earliest_bus(arrival_time: ArrivalTime, busses: Vec<Option<Bus>>) -> (Bus, WaitTime) {
+    let mut best_bus = busses[0].unwrap();
     let mut wait_time = best_bus;
-    for bus in busses[1..].iter() {
-        let min_of_both = best_bus.min(*bus);
+    for bus in busses[1..]
+        .iter()
+        .filter(|c| c.is_some())
+        .map(|c| c.unwrap())
+    {
+        let min_of_both = best_bus.min(bus);
         for i in 0..min_of_both.min(wait_time) {
             let best_bus_wins = ((arrival_time + i) % best_bus) == 0;
             let bus_wins = ((arrival_time + i) % bus) == 0;
@@ -29,7 +32,7 @@ fn get_earliest_bus(arrival_time: ArrivalTime, busses: Vec<Bus>) -> (Bus, WaitTi
             }
             if bus_wins {
                 wait_time = i;
-                best_bus = *bus;
+                best_bus = bus;
                 break;
             }
         }
@@ -56,7 +59,19 @@ mod test {
         let file = read_file("./src/day_13/example.txt");
         let (arrival_time, busses) = from_input(&file);
         assert_eq!(939, arrival_time);
-        assert_eq!(vec![7, 13, 59, 31, 19], busses);
+        assert_eq!(
+            vec![
+                Some(7),
+                Some(13),
+                None,
+                None,
+                Some(59),
+                None,
+                Some(31),
+                Some(19)
+            ],
+            busses
+        );
     }
 
     #[test]
