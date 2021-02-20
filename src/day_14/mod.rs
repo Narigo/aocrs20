2216df60657b685a612af6b2bca75c41089d97f9
Joern Bernhardt <joern.bernhardt@compose.us>
2021-02-20 21:23:36 +0100

    Add day 14 stub
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

---
+++ src/day_14/mod.rs
@@ -0,0 +1,34 @@
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::util::*;
+
+    #[test]
+    fn check_day_14() {}
+
+    #[test]
+    fn check_day_14_star1() {}
+
+    #[test]
+    fn check_day_14_star2() {}
+
+    #[test]
+    fn check_day_14_star1_example() {
+        let file = read_file("./src/day_14/example.txt");
+    }
+
+    #[test]
+    fn check_day_14_star1_input() {
+        let file = read_file("./src/day_14/input.txt");
+    }
+
+    #[test]
+    fn check_day_14_star2_example() {
+        let file = read_file("./src/day_14/example.txt");
+    }
+
+    #[test]
+    fn check_day_14_star2_input() {
+        let file = read_file("./src/day_14/input.txt");
+    }
+}
